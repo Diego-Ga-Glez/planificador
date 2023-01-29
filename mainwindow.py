74e20183d149e1b9f1c5a28f8f72206a9f6cc9cb
Diego Garcia Gonzalez <diego.garcia2084@alumnos.udg.mx>
2023-01-29 01:23:20 -0600

    Slot en boton proceso

--- mainwindow.py
+++ mainwindow.py
@@ -1,6 +1,5 @@
-import sys
-from PySide6 import QtCore, QtWidgets, QtGui
 from PySide6.QtWidgets import QMainWindow
+from PySide6.QtCore import Slot
 from  uipy.ui_mainwindow import Ui_MainWindow
 
 class MainWindow(QMainWindow):
@@ -8,3 +7,10 @@ class MainWindow(QMainWindow):
         super(MainWindow, self).__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+
+        #slots
+        self.ui.procesos_pushButton.clicked.connect(self.num_window)
+
+    @Slot()
+    def num_window(self):
+        print('click')
--- uipy/ui_mainwindow.py
+++ uipy/ui_mainwindow.py
@@ -15,71 +15,43 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QGridLayout, QHeaderView, QLabel,
-    QMainWindow, QMenuBar, QPushButton, QSizePolicy,
-    QSpacerItem, QStatusBar, QTableView, QWidget)
+from PySide6.QtWidgets import (QApplication, QGridLayout, QListView, QMainWindow,
+    QMenuBar, QPushButton, QSizePolicy, QStatusBar,
+    QWidget)
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(772, 629)
+        MainWindow.resize(800, 600)
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
-        self.centralwidget.setEnabled(False)
         self.gridLayout = QGridLayout(self.centralwidget)
         self.gridLayout.setObjectName(u"gridLayout")
-        self.lb_pendientes = QLabel(self.centralwidget)
-        self.lb_pendientes.setObjectName(u"lb_pendientes")
-        self.lb_pendientes.setEnabled(False)
-        self.lb_pendientes.setLayoutDirection(Qt.LeftToRight)
-        self.lb_pendientes.setAutoFillBackground(False)
-        self.lb_pendientes.setAlignment(Qt.AlignCenter)
+        self.pendientes_listView = QListView(self.centralwidget)
+        self.pendientes_listView.setObjectName(u"pendientes_listView")
 
-        self.gridLayout.addWidget(self.lb_pendientes, 0, 0, 1, 1)
+        self.gridLayout.addWidget(self.pendientes_listView, 0, 0, 1, 1)
 
-        self.lb_terminados = QLabel(self.centralwidget)
-        self.lb_terminados.setObjectName(u"lb_terminados")
-        self.lb_terminados.setAlignment(Qt.AlignCenter)
+        self.procesados_listView = QListView(self.centralwidget)
+        self.procesados_listView.setObjectName(u"procesados_listView")
 
-        self.gridLayout.addWidget(self.lb_terminados, 0, 2, 1, 1)
+        self.gridLayout.addWidget(self.procesados_listView, 0, 1, 1, 1)
 
-        self.tb_pendientes = QTableView(self.centralwidget)
-        self.tb_pendientes.setObjectName(u"tb_pendientes")
+        self.terminados_listView = QListView(self.centralwidget)
+        self.terminados_listView.setObjectName(u"terminados_listView")
 
-        self.gridLayout.addWidget(self.tb_pendientes, 1, 0, 3, 1)
+        self.gridLayout.addWidget(self.terminados_listView, 0, 2, 1, 1)
 
-        self.tb_terminados = QTableView(self.centralwidget)
-        self.tb_terminados.setObjectName(u"tb_terminados")
-        self.tb_terminados.setShowGrid(True)
-        self.tb_terminados.setGridStyle(Qt.DashLine)
+        self.procesos_pushButton = QPushButton(self.centralwidget)
+        self.procesos_pushButton.setObjectName(u"procesos_pushButton")
 
-        self.gridLayout.addWidget(self.tb_terminados, 1, 2, 3, 1)
-
-        self.btn_procesos = QPushButton(self.centralwidget)
-        self.btn_procesos.setObjectName(u"btn_procesos")
-        sizePolicy = QSizePolicy(QSizePolicy.Minimum, QSizePolicy.Minimum)
-        sizePolicy.setHorizontalStretch(0)
-        sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.btn_procesos.sizePolicy().hasHeightForWidth())
-        self.btn_procesos.setSizePolicy(sizePolicy)
-
-        self.gridLayout.addWidget(self.btn_procesos, 3, 1, 1, 1)
-
-        self.verticalSpacer = QSpacerItem(20, 40, QSizePolicy.Minimum, QSizePolicy.Expanding)
-
-        self.gridLayout.addItem(self.verticalSpacer, 2, 1, 1, 1)
-
-        self.tb_procesados = QTableView(self.centralwidget)
-        self.tb_procesados.setObjectName(u"tb_procesados")
-        self.tb_procesados.setEnabled(False)
-
-        self.gridLayout.addWidget(self.tb_procesados, 1, 1, 1, 1)
+        self.gridLayout.addWidget(self.procesos_pushButton, 1, 1, 1, 1)
 
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QMenuBar(MainWindow)
         self.menubar.setObjectName(u"menubar")
-        self.menubar.setGeometry(QRect(0, 0, 772, 22))
+        self.menubar.setGeometry(QRect(0, 0, 800, 22))
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QStatusBar(MainWindow)
         self.statusbar.setObjectName(u"statusbar")
@@ -91,9 +63,7 @@ class Ui_MainWindow(object):
     # setupUi
 
     def retranslateUi(self, MainWindow):
-        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"Planificador", None))
-        self.lb_pendientes.setText(QCoreApplication.translate("MainWindow", u"Procesos Pendientes: ", None))
-        self.lb_terminados.setText(QCoreApplication.translate("MainWindow", u"Procesos Terminados", None))
-        self.btn_procesos.setText(QCoreApplication.translate("MainWindow", u"Iniciar Procesos", None))
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        self.procesos_pushButton.setText(QCoreApplication.translate("MainWindow", u"Iniciar Procesos", None))
     # retranslateUi
 
